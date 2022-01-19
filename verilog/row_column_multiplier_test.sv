f33a27233aa54275003b404bfbdbe3623c7038c3
Asmer Hamid Ali <asmerali4feb@gmail.com>
2022-01-19 18:29:58 +0530

    Update row_column_multiplier_test.sv

--- verilog/row_column_multiplier_test.sv
+++ verilog/row_column_multiplier_test.sv
@@ -1,17 +1,14 @@
-
-module multest;
-  reg signed [15:0] W;
+module iterate_test;
+  reg signed [47:0]W ;
   reg I;
-  wire signed [15:0] T ;
-  mul DUT (W,I,T);
+  wire signed [47:0]T;
+  iterate DUT (W,I,T);
  initial
-  begin
-   //$dumpfile (".vcd"); $dumpvars (0,alutest);
-    $monitor ($time," W=%d, I=%b, T=%d", W, I, T);
-    #5 I = 1; W = -16'd34;
-     #5 I = 0; W = 16'd44;
-    #5 I = 1; W = 16'd24;
-    #5 I = 0; W = -16'd14;
-   #5 $finish;
-  end
+      begin
+        // Apply Inputs
+        $monitor ($time," W1=%d, I=%b, T1=%d", W, I, T);
+        #5 I = 1; W = {16'd1,16'd2,16'd3}; 
+        #5 I = 0; W = {-16'd34,16'd44,16'd24};
+       
+    end
 endmodule
