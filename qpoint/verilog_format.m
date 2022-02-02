b0ce7a96001333cbc95177492971d88381edec41
Mohammad Zain <66618738+MZain-electro@users.noreply.github.com>
2022-02-02 19:31:42 +0530

    Update verilog_format.m

--- qpoint/verilog_format.m
+++ qpoint/verilog_format.m
@@ -1,8 +1,13 @@
 function y = verilog_format(x,a,b)
 %a=number of bits before the decimal point without including the 
 %the sign bit
-%b is number of bits after the decimal
-y=blanks(a+b+2);
-y(1:a+1)=x(1:a+1);
-y(a+2)='_';
-y(a+3:a+b+2)=x(a+2:a+b+1);
+%b is number of bits after
+%y=blanks(length(x),a+b+2);
+for i=1:size(x,1)
+y(i,1:a+1)=x(i,1:a+1);
+y(i,a+2)='_';
+y(i,a+3:a+b+2)=x(i,a+2:a+b+1);
+end
+
+
+%%%%note that the input should always be in column vector
